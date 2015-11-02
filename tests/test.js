9f56f1f285fce75c302c77e82a8c02bc2010370b
andreas.lindgren <andreas.lindgren@outlook.com>
2015-11-02 14:39:58 +0100

    redid test, added npm test

--- tests/test.js
+++ tests/test.js
@@ -7,4 +7,20 @@ test('Check init', function (t) {
   animationLoader.init('../animations', 16, 16, function (err) {
     t.error(err, 'initialize loader OK!')
   })
+
+
+
+
+  var a = new UintArray(1)
+  var b = new Uint8Array(1)
+
+  a[0] = 0xffffff
+  b[0] = 0xffffff
+
+
+
+
+  console.log(a, b);
+
+
 })

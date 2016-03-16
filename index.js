00c08c2dc6a3265dd1c79b7d9d69ae395b28e2b7
Andreas Lindgren <andreas.lindgren@outlook.com>
2016-03-16 18:35:18 +0000

    index.js cleanup

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict'
 var animationLoader = require('./animation-loader.js')
 var animationPlayer = require('./animation-player.js')
-// var ws28x = require('rpi-ws281x-native')
 var gpioHandler = require('./gpio-handler.js')
 var server = require('./server.js')
 
-  // gpioHandler.init(animationPlayer)
 animationLoader.init(16, 16, function (err, animations) {
   animationPlayer.init(animations, 16, 16, function(err) {
       if(err) throw err
